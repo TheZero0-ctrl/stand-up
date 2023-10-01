a4441cdd04914c738615c935e9d54587fe3e1391
true-ankit <ankit@truemark.com.np>
2023-10-01 22:51:37 +0545

    heighlight weekends when creating new worksheet

--- stand_up/sheet_api.py
+++ stand_up/sheet_api.py
@@ -4,7 +4,7 @@
 
 import pygsheets
 import typer
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 client = pygsheets.authorize(
     service_account_file="timelog-379717-26592c971bb5.json")
@@ -29,7 +29,7 @@ def get_or_create_month_worksheet(spreadsheet, month):
     return worksheet
 
 def create_month_worksheet(spreadsheet, month):
-    worksht = spreadsheet.add_worksheet(month)
+    worksht = spreadsheet.add_worksheet(month, index=0)
     worksht.update_col(1, [["Date"], ["In Office"], ["Task"], ["hrs"]])
     worksht.adjust_column_width(3, pixel_size=520)
 
@@ -38,6 +38,32 @@ def create_month_worksheet(spreadsheet, month):
     date_values = [[date.strftime('%m/%d/%Y')] for date in dates]
 
     worksht.update_row(2, date_values)
+
+    num_rows = len(date_values) + 1
+    cell_updates = []
+
+    for row_num in range(2, num_rows + 1):
+        date_cell = worksht.cell((row_num, 1))
+        date_value = datetime.strptime(date_cell.value, '%m/%d/%Y')
+        if date_value.weekday() == 5 or date_value.weekday() == 6:
+            row_range = f"A{row_num}:Z{row_num}"
+            cell_updates.append(
+                {
+                    "repeatCell": {
+                        "range": worksht.get_gridrange(f"A{row_num}", f"Z{row_num}"),
+                        "cell": {
+                            "userEnteredFormat": {
+                                "backgroundColor": {"red": 1, "green": 0.65, "blue": 0}
+                                }
+                            },
+                        "fields": "userEnteredFormat.backgroundColor",
+                        }
+                    }         
+                )
+
+    # Batch update cell formatting
+    if cell_updates:
+        client.sheet.batch_update(spreadsheet.id, cell_updates)
     return worksht
 
 def first_and_last_day_of_month():
